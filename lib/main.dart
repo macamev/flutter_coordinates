8a7386602fe0ce1c684b496dc994ba5ab935d32e
macamev <shmaxim@tuta.io>
2025-01-11 19:04:52 +0200

    maze navigation with elevated buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,9 @@ import 'package:get/get.dart';
 main() {
 runApp(
   GetMaterialApp(
-    initialRoute: "/",
-    getPages: [
-      GetPage(name: "/", page: () => StepScreen()),
-      GetPage(name: "/steps/:stepId", page: () => StepScreen()),
+      initialRoute: "/x/0/y/0",  // Start at (0, 0)
+      getPages: [
+        GetPage(name: "/x/:x/y/:y", page: () => StepScreen()),
     ],
   ),
 );
@@ -16,13 +15,48 @@ runApp(
 class StepScreen extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
-  int step = Get.parameters["stepId"] != null ? int.parse(Get.parameters["stepId"]!) : 0;
+  // Get the x and y coordinates from the URL parameters
+    int xCoord = int.parse(Get.parameters["x"]!);
+    int yCoord = int.parse(Get.parameters["y"]!);
+
   return Scaffold(
+  appBar: AppBar(title: Text("Maze Navigation")),
     body: Center(
-      child: OutlinedButton(
-        onPressed: () => Get.toNamed("/steps/${step + 1}"),
-        child: Text("To step ${step + 1}"),
-      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+        children: [
+          Text(
+              "You are at ($xCoord, $yCoord)", 
+              style: TextStyle(fontSize: 24),
+            ),
+
+          ElevatedButton(
+          onPressed: () => Get.toNamed("/x/$xCoord/y/${yCoord + 1}"),
+          child: Text("Up"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+              onPressed: () => Get.toNamed("/x/${xCoord - 1}/y/$yCoord"),
+              child: Text("Left"),
+              ),
+
+              ElevatedButton(
+              onPressed: () => Get.toNamed("/x/$xCoord/y/${yCoord - 1}"),
+              child: Text("Down"),
+              ),
+
+              ElevatedButton(
+              onPressed: () => Get.toNamed("/x/${xCoord + 1}/y/$yCoord"),
+              child: Text("Right"),
+              ),
+            ]
+            
+          )
+      ],
+      )
+      
     ),
   );
 }
